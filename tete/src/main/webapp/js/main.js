b1f0163abf883d30f9f25cc6648cc3e656c2304a
kimtg <kimtg@DESKTOP-KB2D366>
2020-06-03 17:34:59 +0900

    20200603 kimtg css/js update

--- tete/src/main/webapp/js/main.js
+++ tete/src/main/webapp/js/main.js
@@ -1,6 +1,6 @@
 (function ($)
   { "use strict"
-  
+
 /* 1. Proloder */
     $(window).on('load', function () {
       $('#preloader-active').delay(450).fadeOut('slow');
@@ -27,7 +27,7 @@
       }, 800);
       return false;
     });
-  
+
 
 /* 3. slick Nav */
 // mobile_menu
@@ -57,7 +57,7 @@
         autoplaySpeed: 4000,
         dots: false,
         fade: true,
-        arrows: false, 
+        arrows: false,
         prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
         responsive: [{
@@ -150,8 +150,8 @@
         }
       ]
       });
-      
-     
+
+
     // Brand Active
     $('.brand-active').slick({
       dots: false,
@@ -270,8 +270,8 @@
     new WOW().init();
 
 
-    
-// 11. ---- Mailchimp js --------//  
+
+// 11. ---- Mailchimp js --------//
     function mailChimp() {
       $('#mc_embed_signup').find('form').ajaxChimp();
     }
